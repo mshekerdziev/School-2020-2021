c1e895e2e26d1f286fb98ed452d3120f21b23b02
mshekerdziev <82507819+mshekerdziev@users.noreply.github.com>
2021-04-28 13:16:33 +0300

    Add files via upload

---
+++ YearHomework.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+int year;
+cout<<" Enter the year of your coice "<<endl;
+cin>>year;
+if(year%4==0 && year%100!=0) cout<<" The year is leap " <<endl;
+else cout<< " The year is not leap "<<endl;
+return 0;
+}
