3fd486686bb2470d3558a850dcb327af6587676c
mshekerdziev <82507819+mshekerdziev@users.noreply.github.com>
2021-05-05 10:34:46 +0300

    Update YearHomework.cpp

--- YearHomework.cpp
+++ YearHomework.cpp
@@ -4,7 +4,7 @@ int main () {
 int year;
 cout<<" Enter the year of your coice "<<endl;
 cin>>year;
-if(year%4==0 && year%100!=0) cout<<" The year is leap " <<endl;
+if(year%4==0 && year%100!=0 or year%400==0) cout<<" The year is leap " <<endl;
 else cout<< " The year is not leap "<<endl;
 return 0;
 }
